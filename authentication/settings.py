624fe5e82d0fb956abc655569e74309bfeff4248
madhu2904 <madhumithavelusamy0@gmail.com>
2024-03-14 23:44:30 +0530

    final commit

--- authentication/settings.py
+++ authentication/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.office365.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = "madhumithavelusamy0@outlook.com"  # Your Outlook email address
-EMAIL_HOST_PASSWORD = "madhu@123"  # Your Outlook email password or app password
+EMAIL_HOST_USER = ""  # Your Outlook email address
+EMAIL_HOST_PASSWORD = ""  # Your Outlook email password or app password
